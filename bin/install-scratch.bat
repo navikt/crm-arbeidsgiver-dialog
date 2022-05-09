d36708609704b0befb6f26b610ae69a141514afe
Jordan Mathews <58025394+jordan-mathews@users.noreply.github.com>
2022-05-09 08:35:16 +0200

    Added build scripts

---
+++ bin/install-scratch.bat
@@ -0,0 +1,68 @@
+@echo OFF
+
+rem Set parameters
+set ORG_ALIAS=arbeidsgiver-dialog
+
+
+@echo:
+echo Installing crm-arbeidsgiver-dialog scratch org (%ORG_ALIAS%)
+@echo:
+
+echo Cleaning previous scratch org...
+cmd.exe /c sfdx force:org:delete -p -u %ORG_ALIAS% 2>NUL
+@echo:
+
+echo Creating scratch org...
+cmd.exe /c sfdx force:org:create -s -f config/project-scratch-def.json -d 7 -a %ORG_ALIAS%
+call :checkForError
+@echo:
+
+echo Installing dependencies...
+secret=$(jq '.PACKAGE_KEY' env.json -r)
+keys="" && for p in $(jq '.packageAliases | keys[]' sfdx-project.json -r); do keys+=%p%":"%secret%" "; done 
+cmd.exe /c sfdx sfpowerkit:package:dependencies:install -u %ORG_ALIAS% -r -a -w 60 -k "%{keys}%"
+call :checkForError
+@echo:
+
+echo Pushing metadata...
+cmd.exe /c sfdx force:source:push
+call :checkForError
+@echo:
+
+echo Assigning permissions...
+cmd.exe /c sfdx force:user:permset:assign -n Messaging_Read_and_Write_Messages_and_Threads
+call :checkForError
+@echo:
+
+echo Inserting test data...
+cmd.exe /c sfdx force:data:tree:import -p  dummy-data/plan.json
+call :checkForError
+@echo:
+
+echo Activating Mocks...
+cmd.exe /c sfdx force:apex:execute -f ./scripts/apex/activateMock.cls
+call :checkForError
+@echo:
+
+echo Opening org...
+cmd.exe /c sfdx force:org:open
+@echo:
+
+
+
+rem Report install success if no error
+@echo:
+if ["%errorlevel%"]==["0"] (
+  echo Installation completed.
+  @echo:
+)
+
+:: ======== FN ======
+GOTO :EOF
+
+rem Display error if the install has failed
+:checkForError
+if NOT ["%errorlevel%"]==["0"] (
+    echo Installation failed.
+    exit /b %errorlevel%
+)
