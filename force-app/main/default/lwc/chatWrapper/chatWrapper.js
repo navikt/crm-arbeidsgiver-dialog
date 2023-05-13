5d26f705b145a272b3e59996fc1021c01cb93deb
piszosta <piotrszostak@gmail.com>
2023-05-13 05:30:54 +0200

    Added Term Cache functionality

--- force-app/main/default/lwc/chatWrapper/chatWrapper.js
+++ force-app/main/default/lwc/chatWrapper/chatWrapper.js
@@ -1,22 +1,54 @@
-import { LightningElement, api } from 'lwc';
+import { LightningElement, api, track, wire } from 'lwc';
+import { CurrentPageReference } from 'lightning/navigation';
+import getCacheExpired from '@salesforce/apex/EmployerThreadDialogHelper.getCacheExpired';
+import saveTermsAccepted from '@salesforce/apex/EmployerThreadDialogHelper.saveTermsAccepted';
+import USER_ID from '@salesforce/user/Id';
 
 export default class ChatWrapper extends LightningElement {
     @api recordId;
     termsAccepted = false;
+    currentPageReference = null;
+    @track agreementNumber;
+    @track currentUserId = USER_ID;
+
+    @wire(CurrentPageReference)
+    getStateParameters(currentPageReference) {
+        if (currentPageReference) {
+            this.currentPageReference = currentPageReference;
+        }
+    }
+
+    connectedCallback() {
+        this.agreementNumber = this.getUrlParameter('avtalenummer');
+        this.checkCacheExpired();
+    }
 
     handleAccept() {
         this.termsAccepted = true;
+        this.sendTermsAccepted();
     }
 
     handleReject() {
         this.termsAccepted = false;
     }
 
-    // TODO:
-    /*
-    Save result to either session storage or user table.
-    Clarify how often the terms need to be accepted.
-    Add method to check if the terms were previously accepted and still valid on componenet load.
-    Update html with final text
-    */
+    checkCacheExpired() {
+        getCacheExpired({ agreementNumber: this.agreementNumber, userId: this.currentUserId })
+            .then((result) => {
+                this.termsAccepted = result;
+            })
+            .catch((error) => {
+                console.error('checkCacheExpired error', error);
+            });
+    }
+
+    sendTermsAccepted() {
+        saveTermsAccepted({ contractNumber: this.agreementNumber, user: this.currentUserId }).catch((error) => {
+            console.error('sendTermsAccepted error', error);
+        });
+    }
+
+    getUrlParameter(paramName) {
+        return this.currentPageReference.state[paramName];
+    }
 }
